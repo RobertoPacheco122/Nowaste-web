b940f908ad6dfcbf2b9fa242b911c91fb96fd743
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2025-05-20 18:41:18 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./global.css";
+
+export const App = () => {
+  return (
+    <div className="min-h-screen bg-amber-400">
+      <h1 className="font-semibold">App</h1>
+    </div>
+  );
+};
