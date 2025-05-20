5567ed3ac0d98ed17a771e7466e16c6efc7feaa8
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2025-05-20 20:21:44 -0300

    Feat: Adds the app layout

---
+++ src/pages/_layout/app.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Outlet } from "react-router";
+import { Header } from "@/components/header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export const AppLayout = () => {
+  return (
+    <div className="h-screen flex">
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="grow">
+          <Header />
+          <main className="p-6">
+            <Outlet />
+          </main>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import { SignIn } from "./pages/auth/sign-in";
 import { AuthLayout } from "./pages/_layout/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { ChartColumnIncreasing } from "lucide-react";
+import { AppLayout } from "./pages/_layout/app";
+import { Dashboard } from "./pages/app/Dashboard";
 
 export const authRoutes = [
   {
@@ -20,7 +23,25 @@ export const authRoutes = [
   },
 ];
 
+export const applicationRoutes = [
+  {
+    name: "Dashboard",
+    icon: ChartColumnIncreasing,
+    route: {
+      path: "/",
+      element: <Dashboard />,
+    },
+  },
+];
+
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: applicationRoutes.map(({ route }) => {
+      return route;
+    }),
+  },
   {
     path: "/",
     element: <AuthLayout />,
